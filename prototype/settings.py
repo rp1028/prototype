5b0b83ea27647cb5ffad22fc23f528818cf11594
moong <moongunu@gmail.com>
2025-06-14 00:53:48 +0900

    프론트연동 후 회원가입까지 확인 완료 but 추가적인 정보 입력칸 필요해보임

--- prototype/settings.py
+++ prototype/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'accounts',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'prototype.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
