c3b3e431b135c6b799e9b4b9969c09c14ef02d57
moong <moongunu@gmail.com>
2025-06-16 12:56:01 +0900

    게시글 조회 및 정렬 작성 완료

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostCreateView
-
+from posts.views import PostCreateView
+from posts.views import PostListView
 urlpatterns = [
     path('create-post/', PostCreateView.as_view(), name='create-post'),
+    path('list-posts/', PostListView.as_view(), name='list-posts'),  
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import PostSerializer
+from .models import Post
 
 class PostCreateView(APIView):
     permission_classes = [IsAuthenticated]
@@ -13,3 +17,10 @@ class PostCreateView(APIView):
             serializer.save(author=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class PostListView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_at', 'title']
+    ordering = ['-created_at'] 
