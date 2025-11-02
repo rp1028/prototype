17d91a9635b2cf25f243cc0efedcb58d4a8de8b0
rp1028 <lkw20001028@gmail.com>
2025-11-03 04:19:19 +0900

    Create serializers.py

---
+++ mypage/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Music, MusicLike
+
+
+class MusicAuthorSerializer(serializers.ModelSerializer):
+    """음악 작성자 정보 (간단 버전)"""
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class MusicSerializer(serializers.ModelSerializer):
+    """음악 시리얼라이저"""
+    author = MusicAuthorSerializer(read_only=True)
+    likes_count = serializers.IntegerField(read_only=True)
+    is_liked = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Music
+        fields = ['id', 'title', 'description', 'author', 'audio_file', 
+                  'cover_image', 'genre', 'duration', 'created_at', 
+                  'likes_count', 'is_liked']
+        read_only_fields = ['id', 'created_at', 'author']
+    
+    def get_is_liked(self, obj):
+        """현재 사용자가 좋아요 했는지 확인"""
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return MusicLike.objects.filter(user=request.user, music=obj).exists()
+        return False
+
+
+class FavoriteMusicSerializer(serializers.ModelSerializer):
+    """좋아요한 음악 시리얼라이저"""
+    music = MusicSerializer(read_only=True)
+    
+    class Meta:
+        model = MusicLike
+        fields = ['id', 'music', 'created_at']
+        read_only_fields = ['id', 'created_at']
